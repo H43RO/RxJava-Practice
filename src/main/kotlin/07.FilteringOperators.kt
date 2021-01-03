58149aae86f1ee3e2702b4e241b88a81ddb56cee
H43RO <h2is1234@naver.com>
2021-01-03 21:32:50 +0900

    Filtering Operators

---
+++ src/main/kotlin/07.FilteringOperators.kt
@@ -0,0 +1,43 @@
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
+
+fun main(){
+    // 필터링 연산자
+
+    // 1. debounce
+    // - 일정 시간 다른 아이템이 생성되지 않으면 데이터를 전달하는 연산자
+    // - ex) 사용자 키보드 입력대로 자동 API 호출 등
+
+    Observable.interval(250, TimeUnit.MILLISECONDS)
+        .debounce(333, TimeUnit.MILLISECONDS)
+        .subscribe(::println)
+    Thread.sleep(1000)
+
+
+    // 2. distinct / distinctUntilChange
+    // - distinct : 중복되지 않는 데이터만 전달
+    // - distinctUntilChange : 연속하여 같은 값은 전달하지 않음
+
+    data class T(val a: Int, val b: Int)
+    Observable.just(T(2, 3), T(2, 4), T(1, 5), T(7, -1), T(2, 2))
+        .distinct { it.a + it.b }  // Key Selector : 예제에서는 두 수의 합이 이전에 나왔던 합이면 거름
+        .subscribe(::println)
+
+
+    // 3. filter / ofType
+    // - filter : 특정 조건에 맞는 데이터만 전달
+    // - ofType : 특정 타입에 맞는 데이터만 전달. 전달 시 Type Casting 이 되어있음
+
+    Observable.just(11, true, "Hello", "Rx", false)
+        .ofType(String::class.java)
+        .filter{ it.length == 2}
+        .subscribe(::println)
+
+
+    // 4. ignoreElements
+    // -  모든 데이터를 무시하고 Completable 을 리턴 (데이터 받긴 하지만 성공여부만 알고 싶음)
+
+    Observable.just(6, 4, 2)
+        .ignoreElements()
+        .subscribe(::println)
+}
